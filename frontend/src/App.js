6ca0127759a878a7c1c4cab1595ccf405df0f5a8
Jin-young0609 <N12574422@qut.edu.au>
2026-04-04 22:23:56 +1000

    feat: update App.js routing and Navbar links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,26 @@ import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Tasks from './pages/Tasks';
+import Groups from './pages/Groups';
+import MyGroups from './pages/MyGroups';
+import CreateGroup from './pages/CreateGroup';
+import EditGroup from './pages/EditGroup';
+import AdminPanel from './pages/AdminPanel';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/my-groups" element={<MyGroups />} />
+        <Route path="/create-group" element={<CreateGroup />} />
+        <Route path="/edit-group/:id" element={<EditGroup />} />
+        <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </Router>
   );
