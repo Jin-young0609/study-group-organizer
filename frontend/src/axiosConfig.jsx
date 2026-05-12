d14752ea8462358b5765cd29c3b9b848b375f060
Jin-young0609 <N12574422@qut.edu.au>
2026-05-12 13:20:44 +1000

    config: update axios baseURL to new EC2 IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-baseURL: 'http://3.27.185.147', // live
+baseURL: 'http://15.134.145.245', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
